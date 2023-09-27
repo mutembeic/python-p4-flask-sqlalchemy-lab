bf56de6caf6d9a198e76b18ca6cd7ce4f7ce85c5
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-09-27 11:58:18 +0300

    done

---
+++ app/app.py
@@ -0,0 +1,34 @@
+ 
+from flask import Flask, make_response, render_template
+from flask_migrate import Migrate
+from app.models import db, Zookeeper, Enclosure, Animal
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return '<h1>Zoo app</h1>'
+
+@app.route('/animal/<int:id>')
+def animal_by_id(id):
+    animal = Animal.query.get(id)
+    return render_template('animal.html', animal=animal)
+
+@app.route('/zookeeper/<int:id>')
+def zookeeper_by_id(id):
+    zookeeper = Zookeeper.query.get(id)
+    return render_template('zookeeper.html', zookeeper=zookeeper)
+
+@app.route('/enclosure/<int:id>')
+def enclosure_by_id(id):
+    enclosure = Enclosure.query.get(id)
+    return render_template('enclosure.html', enclosure=enclosure)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
